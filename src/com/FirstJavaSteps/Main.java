99988d25d22b65d0f00226be8e73810e2ce756dd
rouanthompson <rouan.thompson@gmail.com>
2021-04-12 17:17:02 -0400

    Calculates mortgage with currency symbol, and up to the hundredth place

--- src/com/FirstJavaSteps/Main.java
+++ src/com/FirstJavaSteps/Main.java
@@ -1,8 +1,38 @@
 package com.FirstJavaSteps;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        float   annualInterestRate;
+        byte    period;
+        long    principal;
+        double  mortgage;
+        float   monthlyRate;
+        int     numberOfPayments;
+        String  result;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        principal = scanner.nextLong();
+        System.out.print("Annual Interest Rate: ");
+        annualInterestRate = (float) scanner.nextDouble();
+        System.out.print("Period (Years): ");
+        period = scanner.nextByte();
+
+        // Calculates mortgage
+
+        monthlyRate = ( annualInterestRate / 100 ) / 12;
+        numberOfPayments = period * 12;
+        mortgage =  ( principal * ( monthlyRate * ( Math.pow( 1 + monthlyRate, numberOfPayments ) /
+                ( Math.pow( 1 + monthlyRate, numberOfPayments ) - 1 ))));
+
+        //
+
+        result = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + result);
     }
 }
